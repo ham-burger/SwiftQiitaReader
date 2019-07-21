c74db5abecdad04444bf1a26ab5624cf87cf219a
kudooooo <devkudou@gmail.com>
2019-07-21 21:07:06 +0900

    Initial Commit

---
+++ SwiftQiitaReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftQiitaReader
+//
+//  Created by kudo on 2019/07/21.
+//  Copyright © 2019 kudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
